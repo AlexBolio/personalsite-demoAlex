dde6ae5fdce9d2bb287eb15042e11109e394a90f
edifactmx-otrejo <edifactmxoscartrejo@gmail.com>
2025-04-07 16:27:59 -0600

    cambios  project controller y template

--- src/main/java/mx/com/edifact/demo/controller/ProjectsController.java
+++ src/main/java/mx/com/edifact/demo/controller/ProjectsController.java
@@ -1,16 +1,18 @@
 package mx.com.edifact.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
  * @author ITIC Roger Azcorra Novelo [RJAN]
  */
+@Controller
 public class ProjectsController {
 
     @GetMapping("/projects")
     public String index() {
         return "projects";
     }
-
+ 
 }
